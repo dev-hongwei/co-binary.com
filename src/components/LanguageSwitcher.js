c96f4c08a1941f80b94ebf2d3dff9f66bf28837a
Hongwei Liu <13699251542.l@gmail.com>
2024-02-25 11:30:35 -0500

    optimize language switcher

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef } from 'react'
-import { Link, useI18next } from 'gatsby-plugin-react-i18next'
+import { Link, useI18next, useTranslation } from 'gatsby-plugin-react-i18next'
 import LanugageDictionary from '../common/LanguageDictionary'
 
 const LanguageSwitcher = ({ onWidthChange }) => {
   const { languages, originalPath, i18n } = useI18next()
+  const { t } = useTranslation()
   const ref = useRef(null)
 
   useEffect(() => {
@@ -14,22 +15,31 @@ const LanguageSwitcher = ({ onWidthChange }) => {
   }, [onWidthChange])
 
   return (
-    <ul ref={ref} className="language-switcher">
-      {languages.map((lng) => {
-        return (
-          <li key={lng}>
-            <Link
-              to={originalPath}
-              language={lng}
-              style={{
-                display: i18n.resolvedLanguage === lng ? 'none' : 'block',
-              }}
-            >
-              {LanugageDictionary[lng]}
-            </Link>
-          </li>
-        )
-      })}
+    <ul ref={ref} className="nav language-switcher">
+      <li className="nav-item">
+        <a href="#">
+          <img src="../images/globe.svg" alt={t(`nav-more`)} />
+        </a>
+        <div className="sub-nav-container">
+          <ul className="sub-nav">
+            {languages.map((lng) => {
+              return (
+                <li key={lng} className="sub-nav-item">
+                  <Link
+                    to={originalPath}
+                    language={lng}
+                    style={{
+                      display: i18n.resolvedLanguage === lng ? 'none' : 'block',
+                    }}
+                  >
+                    {LanugageDictionary[lng]}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </li>
     </ul>
   )
 }
