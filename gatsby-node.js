0db81839b9940691c7a09e9fdf772262d406b12e
Hongwei Liu <13699251542.l@gmail.com>
2024-03-05 15:33:04 -0500

    feat(blog): modify the logic for creating node field 'locale' and add a new node filed 'isDefault'

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,22 @@
+const path = require('path')
+const { defaultLanguage } = require('./src/common/Languages')
+
 exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
 
-  if (node.frontmatter) {
+  if (node.internal.type === 'MarkdownRemark') {
+    const name = path.basename(node.fileAbsolutePath, '.md')
+    const isDefault = name === `index.${defaultLanguage}`
+    const lang = isDefault ? defaultLanguage : name.split('.')[1]
     createNodeField({
       node,
       name: 'locale',
-      value: node.fileAbsolutePath.substr(-5, 2),
+      value: lang,
+    })
+    createNodeField({
+      node,
+      name: 'isDefault',
+      value: isDefault,
     })
   }
 }
